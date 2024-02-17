7099e3e9850e7362d939d5a3e1f678bcffcbd2a5
ttrejosg <tomtrejosgil@gmail.com>
2024-02-17 17:50:37 -0500

    Menu, Booking-Opening and Blog Sections finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
-// Get filter buttons
-const filterButtons = document.querySelectorAll(".filter-buttons button");
+// Menú JS
 
-// Get food items
+// Get filter buttons and food items
+const filterButtons = document.querySelectorAll(".filter-buttons button");
 const foodItems = document.querySelectorAll(".food-item");
 
 // Add event listener to each filter button
@@ -26,7 +26,6 @@ function ocultItem(e) {
     item.classList.add("item-shrink-1");
   });
 
-  // Hacer un settimeout para que se vea la animación
   setTimeout(() => {
     filterItem(e);
   }, 300);
@@ -55,3 +54,40 @@ function filterItem(e) {
 }
 
 filterButtons[0].classList.add("btn-clicked");
+
+// Blog carousel
+const blogCarousel = document.querySelector(".carousel");
+const arrowBtns = document.querySelectorAll(".wrapper button");
+const firstCardWidth = document.querySelector(".card").offsetWidth + 29;
+
+let isDragging = false,
+  startX,
+  startScrollLeft;
+
+arrowBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    blogCarousel.scrollLeft +=
+      btn.id === "btn-left" ? -1 * firstCardWidth : firstCardWidth;
+  });
+});
+
+const dragStart = (e) => {
+  isDragging = true;
+  blogCarousel.classList.add("dragging");
+  startX = e.pageX;
+  startScrollLeft = blogCarousel.scrollLeft;
+};
+
+const dragging = (e) => {
+  if (isDragging)
+    blogCarousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+
+const draggStop = () => {
+  isDragging = false;
+  blogCarousel.classList.remove("dragging");
+};
+
+blogCarousel.addEventListener("mousedown", dragStart);
+blogCarousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", draggStop);
